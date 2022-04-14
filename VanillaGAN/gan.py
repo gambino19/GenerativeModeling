c7ebff608ec6442ea5816d5d0fc2d47e581ab97b
gambino19 <michael.p.gambino@gmail.com>
2022-04-14 00:44:36 -0500

    Update gan.py

--- VanillaGAN/gan.py
+++ VanillaGAN/gan.py
@@ -20,19 +20,37 @@ torch.manual_seed(seed)
 # Data
 class ImageData:
     
-    def __init__(self, root, transforms=None, **kwargs):
+    def __init__(self, root, transform=None, **kwargs):
         """
         Extension for defining and getting image data in ImageFolder object form torch 
         
+        Notes:
+            Transforms is the only kwarg for ImageFolder explicitly defined since
+            leaving default as None automatically applies the ToTensor() transfomation
+            since will be needed for evaluation
+        
         Keyword Arguments:
             root (str): Path to directory adhering to torchvision.datasets.ImageFolder specification
+            transform (torchvision.transforms.transorms.Compose): Transformations to apply on data
             **kwargs: Additional ImageFolder keyword arguments
+            
+        Examples:
+            >>> data = ImageData(root='data/',
+                                 transform=transforms.Compose([
+                                    transforms.Resize(64),
+                                    transforms.CenterCrop(64),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                ]))
         """
         
         self.root = root
         
-        self.transforms = transforms
-        self.data = ImageFolder(self.root, self.transforms, **kwargs)
+        if transform is None:
+            transform = transforms.Compose([transforms.ToTensor()])
+        self.transform = transform
+        
+        self.data = ImageFolder(self.root, self.transform, **kwargs)
         
     def get_dataloader(self, **kwargs):
         """ Return Dataloader Object for model evaluation and training """
@@ -40,29 +58,35 @@ class ImageData:
         return torch.utils.data.DataLoader(self.data, **kwargs)
     
     def visualize(self, idx):
-        """ DOCSTRING """
+        """ 
+        Visualize ImageData by Index
         
-        if self.transforms and any(isinstance(method, transforms.ToTensor) for method in self.transforms.transforms):
-            imgs = torch.zeros(len(idx), *self[idx[0]][0].shape)
-            for i, img in enumerate(idx):
-                imgs[i, :, :, :] = self[img][0]
-        else:
-            imgs = torch.zeros(len(idx), 3, *self[idx[0]][0].size) # TODO: Extend to 1 channel grayscale?
-            for i, img in enumerate(idx):
-                imgs[i, :, :, :] = np.transpose(transforms.ToTensor()(self[img][0]), (1,2,0))
+        Keyword Arguments:
+            idx (int, list): Single index or list of indices to visualize
+        """
         
-        grid = 1
-        while grid**2 <= len(imgs): # Create nearest uniform grid space for visualization
-            grid += 1
+        imgs = torch.zeros(len(idx), *self[idx[0]][0].shape)
+        for i, img in enumerate(idx):
+            imgs[i, :, :, :] = self[img][0]
+        
+        nrow = 1
+        while nrow**2 <= len(imgs): # Create nearest uniform grid space for visualization
+            nrow += 1
             
-        plt.figure(figsize=(grid,grid))
+        print(nrow)
+            
+        plt.figure(figsize=(nrow,nrow))
         plt.axis("off")
         
-        plt.imshow(np.transpose(make_grid(imgs, padding=2, normalize=True).cpu(),(1,2,0)))
+        plt.imshow(np.transpose(make_grid(imgs, nrow=nrow, padding=2, normalize=True).cpu(),(1,2,0)))
         
         
     def __getitem__(self, idx):
-        """ Extend ImageData __getitem__ to ImageData obj """
+        """ Extend ImageFolder __getitem__ to ImageData obj """
         return self.data[idx]
     
-data = ImageData('img_align_celeba/') # transform=transforms.Compose([transforms.ToTensor()])
+    def __len__(self):
+        """ Extend ImageFolder __len__ to ImageData obj """
+        return len(self.data)
+    
+data = ImageData('/') # transform=transforms.Compose([transforms.ToTensor()])
